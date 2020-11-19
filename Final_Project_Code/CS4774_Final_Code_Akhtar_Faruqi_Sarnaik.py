fb1b918bf2d4d15387910351d5520d8f4eefbd6d
Kunaal Sarnaik <kss7yy@virginia.edu>
2020-11-18 21:28:18 -0500

    test2

---
+++ Final_Project_Code/CS4774_Final_Code_Akhtar_Faruqi_Sarnaik.py
@@ -0,0 +1,8 @@
+# Class: CS 4774: Machine Learning Final Project
+# Title: Melanoma Classification
+# Team Member 1: Ramiz Akhtar (rsa5wj)
+# Team Member 2: Rayaan Faruqi (raf9dz)
+# Team Member 3: Kunaal Sarnaik (kss7yy)
+# Professor: Yanjun Qi
+# Due Date: December 6th, 2020
+
