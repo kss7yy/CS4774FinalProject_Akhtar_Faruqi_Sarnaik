0e6f72f5a774c36728298b849ea52b0b71c856a2
Kunaal Sarnaik <kss7yy@virginia.edu>
2020-12-02 02:37:57 -0500

    12/2/2020 Preprocessing

--- Final_Project_Code/CS4774_Final_Code_Akhtar_Faruqi_Sarnaik.py
+++ Final_Project_Code/CS4774_Final_Code_Akhtar_Faruqi_Sarnaik.py
@@ -9,12 +9,15 @@ Due Date: December 6th, 2020
 '''
 #Guide: https://medium.com/ai-in-plain-english/image-processing-and-classification-with-python-and-keras-c368769bde26
 
+#https://towardsdatascience.com/the-4-convolutional-neural-network-models-that-can-classify-your-fashion-images-9fe7f3e5399d
 
 #Standard python imports
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 import sys
+import random
+import imutils
 
 # SKLearn 
 from sklearn.utils import shuffle
@@ -40,14 +43,28 @@ from keras.wrappers.scikit_learn import KerasClassifier
 #https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
 from pathlib import Path
 
+#https://www.pluralsight.com/guides/importing-image-data-into-numpy-arrays
+from PIL import Image #for converting images to grayscale, resizing image, trimming image
+
+#OpenCV Library 
+import cv2
+
+#from google.colab.patches import csv2_imshow
+random.seed(42)
+
+
 #Method to obtain the data from the given path (see above for global paths)
 def get_data(path_name):
     
     #Kunaal Path: "../PROJECT_DATA/skin-lesions"
     #Rayaan Path: "/Volumes/Rayaan_Ext2TB/MachineLearningProject/skin-lesions"
-    #Ramiz Path: ""
+    #Ramiz Path: ""    
 
-    PATH = Path("/Volumes/Rayaan_Ext2TB/MachineLearningProject/skin-lesions")
+    #Kunaal Test Path: "../PROJECT_DATA/skin-lesions-trunc/skin-lesions-trunc"
+    #Rayaan Test Path: "/Volumes/Rayaan_Ext2TB/MachineLearningProject/skin-lesions-trunc"
+    #Ramiz Test Path: ""
+
+    PATH = Path("../PROJECT_DATA/skin-lesions-trunc/skin-lesions-trunc")
     
     #join the path and the specific directory (train/valid/test) we're looking at
     split_path = os.path.join(PATH, path_name) 
@@ -59,44 +76,80 @@ def get_data(path_name):
 
     #loadImages will return an array of image arrays for each path 
     nevus_arr = loadImages(nevus_path)
-    # melanoma_arr = loadImages(melanoma_path)
-    # sb_arr = loadImages(sbrk_path)
-    
+    melanoma_arr = loadImages(melanoma_path)
+    sb_arr = loadImages(sbrk_path)
+
     #set up classification matrices
     nevus_y = [0]*len(nevus_arr)
-    # melanoma_y = [1]*len(melanoma_arr)
-    # sb_y = [2]*len(sb_arr)
+    melanoma_y = [1]*len(melanoma_arr)
+    sb_y = [2]*len(sb_arr)
     
-    #Combine and Shuffle
-    
+    #combine
+    x = np.concatenate((nevus_arr, melanoma_arr, sb_arr), axis=0)
+    y = nevus_y + melanoma_y + sb_y
+    y = np.asarray(y)
 
-    #Extract the xdata and ydata from the shuffled, combined data matrix
-
+    #shuffle the coupled data
+    xData, yData = shuffle(x, y)
 
     #Return statement
-    #return xData, yData
-    return nevus_arr
+    return xData, yData
+
     
 # Method to loadImages in the given path (PRE-PROCESSES images as well!)
 def loadImages(path):
     retArr = []
-    path = Path("/Volumes/Rayaan_Ext2TB/MachineLearningProject/testing_code")
     print("Path is: ", path)
+    width = 256
+    height = 256
     for i in os.listdir(path):
         #Load each image in path
-        print("image is: ", i)
-        img = load_img(os.path.join(path, i))
-        img_arr = img_to_array(img)        
-        retArr.append(img_arr)
+        path_i = os.path.join(path, i)
+        img = cv2.imread(path_i, cv2.IMREAD_COLOR) 
+
+        #Resizing images to 256 x 256
+        #https://www.tutorialkart.com/opencv/python/opencv-python-resize-image/
+        img_resized = cv2.resize(img, (width, height))
+
+        #normalize image pixels from 0 to 1
+        #https://stackoverflow.com/posts/39037135/edit    
+        norm_image = cv2.normalize(img_resized, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+             
+        #data augmentation (rotation)
+        #https://www.pyimagesearch.com/2017/01/02/rotate-images-correctly-with-opencv-and-python/
+        #Rotating in 360 over a continuous range versus in increments of 90???
+        angle = random.randint(1,4)*90
+        # print(angle)
+        rotate_img = imutils.rotate(norm_image, angle)
+
+        # cv2.imshow("Rotatated and resized image", rotated_img) #replace dot with underscore for collab
+
+        rotate_img = np.asarray(rotate_img)
+        retArr.append(rotate_img)
     retArr = np.asarray(retArr)
-    print("------------------------------------------------------")
     print(retArr[0])
-    print(retArr[11])
     return retArr
 
 '''
 #Method to create the CNN Model
-def create_cnn(...):
+def create_cnn(x, y, x_val, y_val, args=None):
+    #create base CNN model
+    model = Sequential()
+    #hidden layer
+    #can try activation as 'relu', 'softmax', 'sigmoid', 'tanh' 
+    model.add(Dense(100), input_shape=(dim1,dim2), activation='relu')
+    
+    #output layer
+    model.add(Dense(10), activation='softmax')
+
+    #look at model summary
+    model
+
+    #compiling sequential model
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+
+    #train model for 3 epochs
+    model.fit(x, y, batch_size=6, epochs=3, validation_data=(x_val, y_val))
 
     return model
 
@@ -107,8 +160,20 @@ def train_cnn(...):
 
 #Method to train and select the CNN Model given the parameters specified.
 def train_and_select_model(...):
+    args = {
+        'batch_size': ...,
+        'num_epochs': ..., 
+    }
 
-    return best_model, best_history
+    best_valid_acc = 0
+    best_hyper_set = 0
+
+    for learning_rate in [0.0001, 0.0005, 0.001, 0.005, 0.01]:
+        for opt in ['adam', 'sgd']:
+            for activation_func in ['softmax', 'relu', 'sigmoid', 'tanh']:
+                #model = create_cnn
+
+    return best_model, best_history, best_valid_acc, best_hyper_set
 
 #Method to evaluate the model based on the train
 def model_evaluation(...):
@@ -118,8 +183,6 @@ def model_evaluation(...):
 def plot_history(...):
 
     return ...
-
-
 '''
 
 #MAIN METHOD
@@ -129,9 +192,7 @@ if __name__ == '__main__':
     valid_path = "valid"
     test_path = "test"
     
-    # x_train, y_train = get_data(train_path)
+    x_train, y_train = get_data(train_path)
     # x_valid, y_valid = get_data(valid_path)
     # x_test, y_test = get_data(test_path)
-
-    nevus = get_data(train_path)
-    print(nevus)
+    
